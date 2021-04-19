aa97e7ac74ee552ce4f949ccf3ce26c676ca3dfe
Duardin <edu.casagrande04@hotmail.com>
2021-04-19 18:28:16 -0300

    Create the main-carousel.js

---
+++ js/components/main-carousel.js
@@ -0,0 +1,30 @@
+/*
+1 - Acessar a janela (browser)
+2 - Pegar o HTML todo
+3 - Pegar o botão
+4 - Saber que o botão está sendo clicado
+
+5 - Acessar a janela
+6 - Pegar o html todo
+7 - Pegar o elements
+8 - Mover o elements para a direita
+
+9 - 
+*/
+
+
+
+const btnRight = document.querySelector('.button-arrow.-right');
+const btnLeft = document.querySelector('.button-arrow.-left');
+const elements = document.querySelector('.elements');
+let pixels = 0
+
+btnRight.addEventListener('click', function() {
+    pixels = pixels + 90;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
+
+btnLeft.addEventListener('click', function() {
+    pixels = pixels - 90;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
